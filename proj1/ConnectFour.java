b67c5414364d70f6a3c85f282529f2fcae6cc22c
AAIZAHMED <aaiza2@umbc.edu>
2017-02-27 18:01:11 -0500

    cleaned

--- proj1/ConnectFour.java
+++ proj1/ConnectFour.java
@@ -1,210 +1,245 @@
 /**
-  * This class is ConnectFour.java. It sets up the board and makes sure everything is right.
-  *	@author Aaiz Ahmed <aaiza2@umbc.edu>
-  * @version Sept 21, 2013
-  * @project CMSC 202 - Fall 2013 - Project #1
-  * @section 06
-*/
-package proj1;
-
-public class ConnectFour {
-//Declaring variables to be used in the class.
-private int row, col;	
-private char [][] board;
-
-/**
- * Constructor - Creates a new instant of ConnectFour class and makes sure board size is not less the 5 by 5.
+ * This class is ConnectFour.java. It sets up the board and makes sure everything is right.
+ *	@author Aaiz Ahmed <aaiza2@umbc.edu>
+ * @version Sept 21, 2013
+ * @project CMSC 202 - Fall 2013 - Project #1
+ * @section 06
  */
-public ConnectFour (int xrow, int ycolm ) {
-	row = xrow;
-	col = ycolm;
- 
-//Setting up the board.	
-if (row < 5) {
-    board = new char [5][col]; }
-else 
-	if (col < 5)  {
-		board = new char [row][5]; }
-    else {	 
-	   board = new char [row][col]; }
 
- // Nested for loops to fill up 2D board array  
-	for (int i = 0; i < board.length; i++) {
-	    for ( int j = 0; j < board[0].length; j++)   {
-		  board[i][j]= '_';  
+package Connect_Four.proj1;
+
+//package proj1;
+
+public class ConnectFour 
+{
+	//Declaring variables to be used in the class.
+	private int rows, cols;	
+	private char [][] board;
+
+	/**
+	 * Constructor: Creates a new instant of ConnectFour class and makes sure 
+	 * board size is not less the 5 by 5.
+	 * @param	xrow:	Number of rows
+	 * @param	ycolm:	Number of columns
+	 */
+	public ConnectFour (int xRow, int yCol ) 
+	{
+		// Confirming the number of rows and columns to be at least 5.
+		if (xRow < 5) 
+		{
+			xRow = 5;
 		}
-	} //loop finishes 
-}
+		if (yCol < 5)
+		{
+			yCol = 5;
+		}
 
-/*
- * This method prints out the board. 
- *
-public void printBoard () {
-	for (int i = 0; i < board.length; i++) {  
-	     for (int j = 0; j < board[0].length; j++) {  
-	             
-	    	 System.out.print(board[i][j] );  }
-	       
-	         System.out.println(); }
-} */
+		rows = xRow;
+		cols = yCol;		
+		board = new char[rows][cols];		
 
-/**
- * Method to assign values to the board based on players choice of column.
- * It checks to make sure it's a valid column number otherwise code is not executed. 
- * If condition is true 'x' is placed for player1 or 'o' for player2 at that position.
- * @param col: the column number where player wants to move.
- * @param player: to tell if it's player1 or player2.
- */
-public void reassign (int col, int player) {
+		// Nested for loops to fill up 2D board array  
+		for (int i = 0; i < rows; i++) 
+		{		
+			for ( int j = 0; j < board[0].length; j++)   
+			{
+				board[i][j]= '_';  
+			}
+		}  
+	}
 
-if (col > 0 || col < this.col) 	
-	
-	// As board has to be filled from bottom to top, this for loop checks that which row is empty in given column
-	// filles it
-		for (int j = row-1; j >= 0 ; j--) {
-			
-			if (board[j][col-1]=='_')  
-			 
-			   if (player == 0) {
-				   board[j][col-1] = 'x';
-				   	  
-			break; }  
-			   else 
-			   {
-				   board[j][col-1] = 'o'; 
-			       break; }
-		     }
+	/**
+	 * Method to assign values to the board based on players choice of column.
+	 * It checks to make sure it's a valid column number otherwise code is not executed. 
+	 * If condition is true 'x' is placed for player 1 or 'o' for player2  at that position.
+	 * @param col: the column number where player wants to move.
+	 * @param player: to tell if it's player1 or player2.
+	 */
+	public void makeMove (int col, int player) 
+	{
+		if (col > 0 && col <= this.cols)
+		{			
+			// The board has to be filled from bottom to top, this for loop checks which 
+			// row is empty in the given column and fills it.			
+			for (int i = rows-1; i >= 0 ; i--) 
+			{
+				if (board[i][col-1] == '_')
+				{
+					if (player == 0) 
+					{
+						board[i][col-1] = 'x';
+						return; 
+					}  
+					else 
+					{
+						board[i][col-1] = 'o'; 
+						return; 
+					}
+				}
+			}
+		}
 	} 
-/**
-* To see if column given by user is already full or not. 
-* It also makes sure input is valid. If invalid column/input is entered code doesn't execute. 
-* @param col: column number given by a player.
-*/
-  public boolean columnCheck ( int col) {
-	  
-	  if (col > 0 || col < this.col)
-	
-		if (board[0][col-1]=='x' || board[0][col-1]=='o') {
-			return true; } 
-		
+
+	/**
+	 * To see if column given by user is already full or not. 
+	 * It also makes sure input is valid. 
+	 * If invalid column/input is entered code doesn't execute. 
+	 * @param col: column number given by a player.
+	 */
+	public boolean columnCheck (int col) 
+	{
+		if (col > 0 && col <= this.cols)
+		{
+			// Only check the top row at index Zero
+			if (board[0][col-1] == 'x' || board[0][col-1] == 'o') 
+			{	
+				return true;	
+			} 
+		}
+
 		return false;
-}
+	}
 
-/**
- * This methods checks the horizontal win condition. 
- * @return true or false
- */
-public boolean horizontalWin () {
+	/**
+	 * This methods checks the horizontal win condition. 
+	 * @return true or false
+	 */
+	public boolean horizontalWin () 
+	{
+		for (int i = 0; i < rows; i++) 
+		{   
+			// Go up to col-3 so that we don't get out of bound while checking the next 3 symbols
+			for (int j = 0; j < cols-3; j++) 
+			{
+				if (board[i][j] == 'x' && board[i][j+1]=='x' && board[i][j+2]=='x' && board[i][j+3]=='x') 
+				{
+					return true; 
+
+				}
+				else if (board[i][j] == 'o' && board[i][j+1]=='o' && 
+						 board[i][j+2]=='o' && board[i][j+3]=='o') 
+				{
+					return true;
+				}
+			}
+		} 
+
+		return false;	 
+	}
 	
- 	
-	for (int i = 0; i < row; i++) 
-	{   for (int j = 0; j < col; j++) 
-	   {
-			if (board[i][j]=='x' && (j+1 < col && j+2 < col && j+3 < col)) {
-				if (board[i][j+1]=='x' && board[i][j+2]=='x' && board[i][j+3]=='x') {
-					return true; }   
+	/**
+	 * This method checks the vertical win condition. 
+	 * @return true or false
+	 */
+	public boolean verticalWin () 
+	{
+		for (int i = 0; i < cols; i++) 
+		{   
+			for (int j = 0; j < rows-3; j++) 
+			{
+				if (board[j][i]=='x' && board[j+1][i]=='x' && board[j+2][i]=='x' && board[j+3][i]=='x' ) 
+				{
+					return true;  
 				}
-		else 
-				if (board[i][j]=='o' && (j+1 < col && j+2 < col && j+3 < col) ) {
-					if (board[i][j+1]=='o' && board[i][j+2]=='o' && board[i][j+3]=='o')
-						return true; } 
-		}
-		 
-	} 
-	return false;	 
-}
-/**
- * This method checks the vertical win condition. 
- * @return true or false
- */
-public boolean verticalWin () {
-	for (int i = 0; i < col; i++) 
-	{   for (int j = 0; j < row; j++) 
-	   {
-			if (board[j][i]=='x' && (j+1 < row && j+2 < row && j+3 < row) ) {
-				if (board[j+1][i]=='x' && board[j+2][i]=='x' && board[j+3][i]=='x') {
-					return true; }   }
-		else 
-				if (board[j][i]=='o' && (j+1 < row && j+2 < row && j+3 < row)) {
-					if (board[j+1][i]=='o' && board[j+2][i]=='o' && board[j+3][i]=='o')
-						return true; } 
-		}
-		 
-	} 
-	return false;	
-}
-/**
- * This method checks the Diagonal win. From top left corner to bottom right corner. 
- * @return true or false
- */
-
-public boolean diagonallWinOne () {
-	for (int i = 0; i < row-3; i++) 
-	{   for (int j = 0; j < col-3; j++) 
-	   {
-			if (board[i][j]=='x') {
-				if (board[i+1][j+1]=='x' && board[i+2][j+2]=='x' && board[i+3][j+3]=='x') {
-					return true; }   
+				else if (board[j][i]=='o' && board[j+1][i]=='o' && board[j+2][i]=='o' && board[j+3][i]=='o') 
+				{
+					return true;
 				}
-		else 
-				if (board[i][j]=='o') {
-					if (board[i+1][j+1]=='o' && board[i+2][j+2]=='o' && board[i+3][j+3]=='o')
-						return true; } 
-		}
-		 
-	} 
-	return false;	
-}
-/**
- * This method checks for other case of the Diagonal win.  From Bottom left corner to top right corner. 
- * @return true or false
- */
-public boolean diagonallWinTwo () {
+			}
+		} 
+		
+		return false;	
+	}
+	
+	/**
+	 * This method checks the Diagonal win. From top left corner to bottom right corner. 
+	 * @return true or false
+	 */
 
-	for (int i = row-1; i >= 3; i--) 
-	{   for (int j = 0; j < col-3; j++) 
-	   {
-			if (board[i][j]=='x') {
-				if (board[i-1][j+1]=='x' && board[i-2][j+2]=='x' && board[i-3][j+3]=='x') {
-					return true; }   
+	public boolean diagonallWinOne () 
+	{
+		for (int i = 0; i < rows-3; i++) 
+		{   
+			for (int j = 0; j < cols-3; j++) 
+			{
+				if (board[i][j]=='x' && board[i+1][j+1]=='x' && board[i+2][j+2]=='x' && board[i+3][j+3]=='x') 
+				{		
+					return true; 
+				}   
+				else if (board[i][j]=='o' && board[i+1][j+1]=='o' && board[i+2][j+2]=='o' && board[i+3][j+3]=='o') 
+				{
+					return true;
+				}				 
+			}
+		}
+
+		return false;	
+	}
+	
+	/**
+	 * This method checks for the other case of the Diagonal win.  
+	 * From Bottom left corner to top right corner. 
+	 * @return true or false
+	 */
+	public boolean diagonallWinTwo () 
+	{
+		for (int i = rows-1; i >= 3; i--) 
+		{   
+			for (int j = 0; j < cols-3; j++) 
+			{
+				if (board[i][j]=='x' && board[i-1][j+1]=='x' && board[i-2][j+2]=='x' && board[i-3][j+3]=='x') 
+				{
+					return true;    
 				}
-		else 
-				if (board[i][j]=='o') {
-					if (board[i-1][j+1]=='o' && board[i-2][j+2]=='o' && board[i-3][j+3]=='o')
-						return true; } 
+				else if (board[i][j]=='o' && board[i-1][j+1]=='o' && board[i-2][j+2]=='o' && board[i-3][j+3]=='o') 
+				{
+					return true; 
+				} 
+			}
+		} 
+		
+		return false;	
+	}
+	
+	/**
+	 * This method combines all the winning conditions and returns true if 
+	 * any of them is true and false otherwise.
+	 * @return true or false
+	 */
+	public boolean allWins() 
+	{
+		if (horizontalWin() || verticalWin() || diagonallWinOne() || diagonallWinTwo() ) 
+		{
+			return true; 
 		}
-		 
-	} 
-	return false;	
-}
-/**
- * This method combines all the winning conditions and returns true if any of them is true and false otherwise.
- * @return true or false
- */
+		
+		else 
+			return false;
+	}
 
-public boolean allWins () {
-	if (horizontalWin () || verticalWin () || diagonallWinOne () || diagonallWinTwo () ) {
-		return true; }
-	else
-	return false;
-}
-/**
- * This method is used to print the board as a string.
- * It prints the board and allows the players to see their current and previous moves.
- */
-public String toString () {
-	String boardString = "";
-	for (int i = 0; i < board.length; i++) {  
-	     for (int j = 0; j < board[0].length; j++) {  
-	             
-	    	 boardString +=  board[i][j]  ;  }
-	       
-	         boardString += "\n";
-	 	
-	         }
-			
-	return boardString; 
-}
+	/**
+	 * This method is used to print the board as a string.
+	 * It prints the board and allows the players to see their current and previous moves.
+	 */
+	public String toString () 
+	{
+		for (int i = 0; i < rows; i++) 
+		{  
+			for (int j = 0; j < cols; j++) 
+			{
+				System.out.print( board[i][j] );  
+			}			
+			System.out.println();
+		}
+		System.out.println ();
+
+		return ""; 
+	}
+
+	public static void main(String args[])
+	{
+		ConnectFour c = new ConnectFour(5, 5);
+	}
 
 }
 
--- proj1/Project1.java
+++ proj1/Project1.java
@@ -1,164 +1,191 @@
 /**
- * This class is Project1.java. It communicates with ConnectFour.java and the players/users. It controls input and output.
+ * This class is Project1.java. It communicates with ConnectFour.java and the players/users. 
+ * It controls input and output.
  * @author Aaiz Ahmed <aaiza2@umbc.edu>
  * @version Sept 21, 2013
  * @project CMSC 202 - Fall 2013 - Project #1
  * @section 06
  * @param args
  */
-package proj1;
+
+package Connect_Four.proj1;
+//package proj1;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class Project1 {
-	public static void main (String[] args) {
+public class Project1 
+{
+	private static Scanner scan = new Scanner(System.in);
+	
+	private static Pattern p = Pattern.compile("^\\d+$");
+	private static Matcher m; 
 
-		//Declaring variables to be used.
-		int row = 0, col = 0; 	String option;
-		@SuppressWarnings("resource")
-		Scanner scan = new Scanner(System.in);
+	/**
+	 * This method checks if the input given by the user is a number or not
+	 * @param 	input
+	 * @return  true or false
+	 */
+	public static boolean isNumber(String input)
+	{
+		m = p.matcher(input);
+		return m.matches();
+	}
+	
+	/**
+	 * This method run a full game of Connect Four. It uses a for loop and helper methods.
+	 * @param board		A Connect Four board
+	 * @param rows		Number of rows
+	 * @param cols		Number of columns
+	 */
+	public static void startGame(ConnectFour board, int rows, int cols)
+	{
+		int colNum = 0;
 
-		System.out.println ("Hello and welcome!\nDo you want to play Connect Four game? Press enter or any key to continue and Q/q to quit. ");
-		option = scan.nextLine();
+		// For loop is used to execute total number of moves and alternating between the players.
+		for (int i = 0; i < rows*cols; i++) 
+		{
+			if (i%2 == 0) 
+			{
+				System.out.println("Player 1: What is your choice?");
 
-		//Outer most while loop. Will be used to start a new game if it's a win or a draw.
-		while (!option.equalsIgnoreCase("q")) {
-			System.out.println ("Please enter the number of rows, it can't be less then 5." );
-			row = scan.nextInt();
+				// Use isValid() method to check if the column number given by the user is valid.
+				colNum = isValid(board, cols, 1);			
 
-			while (row < 5) {
-				System.out.println ("Please enter the number of rows, it can't be less then 5." );
-				row = scan.nextInt(); 
+				// Making a move based on input provided.
+				board.makeMove(colNum, 0);
+				board.toString();				
+
+				if (board.allWins ()) 
+				{ 
+					System.out.println("Player 1: Congratulations!! You won!"); 
+					return;  
+				} 
+			}	  
+			else   
+			{ 
+				System.out.println("Player 2: What is your choice?");
+
+				colNum = isValid(board, cols, 2);
+				board.makeMove(colNum, 1);
+				board.toString();
+
+				if (board.allWins ()) 
+				{ 
+					System.out.println("Player 2: Congratulations!! You won!"); 
+					return;  
+				} 
 			}
-			System.out.println ("Please enter the number of columns, it can't be less then 5." );
-			col = scan.nextInt();
+		}
+		
+		System.out.println("It was a draw!");
+	}
 
-			while (col<5) {
-				System.out.println ("Please enter the number of columns, it can't be less then 5." );
-				col = scan.nextInt();  }
+	/**
+	 * This method checks if the column given by the user to make a move is valid.
+	 * Additionally, it quits the game and program if the user types q.
+	 * 
+	 * @param board		A Connect Four board.
+	 * @param cols		Number of columns
+	 * @param player	Players whose turn it is
+	 * 
+	 * @return			A valid column number as an integer.
+	 */
+	public static int isValid(ConnectFour board, int cols, int player)
+	{
+		String input = "";
 
-			//Instantiating a ConnectFour object.
-			ConnectFour hh = new ConnectFour (row,col); 
-			System.out.println (hh.toString());
-			System.out.println ();
+		while(true)
+		{						
+			System.out.printf ("Enter a valid column between 1 and %d or q to quit the game: \n", cols );
+			input = scan.next();
 
+			if (isNumber(input)) 
+			{
+				int numEntered = Integer.parseInt(input);
+				boolean isFull = board.columnCheck(numEntered);
 
-			int times = 0, stage = 0; String check;
-			//For loop for total number of moves and alternating between the players.
-			for (int i = 0; i < row*col; i++ ) {
-				if (i%2 == 0) {
-					System.out.println ("Player1: Please enter the column where you want to play. Give column number." );
-					check = scan.next();
-					if(check.equalsIgnoreCase("q")) {  stage = 1; break; }
-					else {  
-						times = Integer.parseInt(check);            
+				if (isFull)
+				{
+					System.out.printf ("The column you entered is already full. Pick a different column: ");
+				}
 
-						//Making sure input is valid by while loop.
-						while (times <= 0 || times > col) { 
-							System.out.println ("Player1: Please enter a valid column number. It can't be negative, zero or greater then " + col );
-							check = scan.next();  
-							if (check.equalsIgnoreCase("q")) { 
-								stage = 2; break;} //goes to for loop.
-							else {
-								times = Integer.parseInt(check); }
-						}
-
-						if (stage == 2) {break;} //gets out of for loop
-
-						times = Integer.parseInt(check);
-						// Checking if column entered by player1 is already full or not.   
-						while (hh.columnCheck(times)) {
-							System.out.println ("Player1: This column is already full. Please choose another column." );
-							check = scan.next(); 
-							if (check.equalsIgnoreCase("q")) { stage = 3; break;} //goes to for loop
-							else
-								times = Integer.parseInt(check);
-
-							//If column is full and asked to play in another column and player1 gives an invalid column this while loop will prompt for a valid column.  		 
-							while (times <= 0 || times > col) { 
-								System.out.println ("Player1: Please enter a valid column number. It can't be negative, zero or greater then " + col );
-								check = scan.next(); 
-								if (check.equalsIgnoreCase("q")) {stage = 4; break;} //goes to outer while loop. 
-								else 
-									times = Integer.parseInt(check);} 
-							if (stage==4) { break;} //goes to for loop.
-						} 
-						if (stage==3 || stage == 4) { stage = 5; break;} //gets out of for loop
-
-						//Calling methods to reassign x based on player1's valid input, printing the board and checking for win conditions. 	
-						hh.reassign(times, 0); 
-						System.out.println (hh.toString());  
-						if ( hh.allWins ()) { 
-							System.out.println ("\nPlayer1:"); break;  } 
-					}	
-				}  
-				else   { 
-					System.out.println ("Player2: Please enter the column where you want to play. Give column number." );
-					check = scan.next();
-					if(check.equalsIgnoreCase("q")) {  stage = 6; break; }
-					else {  
-						times = Integer.parseInt(check);            
-
-						//Making sure input is valid by while loop.   
-						while (times <= 0 || times > col) { 
-							System.out.println ("Player2: Please enter a valid column number. It can't be negative, zero or greater then " + col );
-							check = scan.next();  
-							if (check.equalsIgnoreCase("q")) { 
-								stage = 7; break;} //goes to for loop.
-							else {
-								times = Integer.parseInt(check); }
-						}
-
-						if (stage == 7) {break;} //gets out of for loop
-
-						times = Integer.parseInt(check);
-
-						// Checking if column entered by player2 is already full or not.     
-						while (hh.columnCheck(times)) {
-							System.out.println ("Player2: This column is already full. Please choose another column." );
-							check = scan.next(); 
-							if (check.equalsIgnoreCase("q")) { stage = 8; break;} //goes to the for loop
-							else
-								times = Integer.parseInt(check);
-
-							//If column is full and asked to play in another column and player2 gives an invalid column this while loop will prompt for a valid column.		 
-							while (times <= 0 || times > col) { 
-								System.out.println ("Player2: Please enter a valid column number. It can't be negative, zero or greater then " + col );
-								check = scan.next(); 
-								if (check.equalsIgnoreCase("q")) {stage = 9; break;} //goes to while loop. 
-								else 
-									times = Integer.parseInt(check);} 
-							if (stage==9) { break;} //goes to for loop.
-						} 
-						if (stage==8 || stage == 9) { stage = 10; break;}
-					} 	 
-
-					//Calling methods to reassign o based on player2's valid input, printing the board and checking for win conditions.    
-					hh.reassign(times, 1); 
-					System.out.println (hh.toString()); 
-					if (hh.allWins ()) { 
-						System.out.println ("\nPlayer2:"); break;} 
+				if (isFull == false && (numEntered > 0 && numEntered <= cols) )
+				{	
+					return numEntered;
 				}
 			}
-			if (stage > 0){
 
-				if (stage == 1 || stage == 2 || stage == 3 || stage == 4|| stage == 5 ) {
-					System.out.println ("Player1 Quit"); break; }
+			if (input.equals("q"))
+			{
+				// Exist the game and stop the program.
+				System.out.println("Player " + player + " has quit the game!");
+				System.out.println("Game ending...");
+				System.exit(0);
+			}
+		}		
+	}
 
-				else
-					//	System.out.println ("Player2 stage value: " + stage);
-					if (stage == 6 || stage == 7 || stage == 8 || stage == 9|| stage == 10) {
-						System.out.println ("Player2 Quit"); break; }
+	public static void main (String[] args) 
+	{
+		//Declaring variables to be used.
+		int rows = 0, cols = 0; 		
 
+		System.out.println ("Hello and welcome!\nThis game is played between 2 players and a "
+						  + "player can quit anytime by pressing q.\n");
+
+		String input = "";
+
+		//Outer most while loop. Will be used to start a new game if it's a win or a draw.
+		while (!input.equals("q")) 
+		{			 
+			while(true)
+			{
+				System.out.println ("Please enter the number of rows. It can not be less then 5." );
+				input = scan.next();
+
+				// Use isNumber() method to check if the input given by the user is a number.
+				if (isNumber(input) &&  Integer.parseInt(input) >= 5) 
+				{
+					rows = Integer.parseInt(input);
+					break;
+				}				
 			}
 
-			else {
-				if ( hh.allWins ()) {
-					System.out.println ("Congratulations!! You won! Press enter or any key to play again and Q/q to Quit the game. " ); 
-					option = scan.nextLine();}
-				else
-					System.out.println ("It's a draw. Do you want to play again? Press enter or any key to play again and Q/q to Quit the game. " );
-				option = scan.next();}
+			while(true)
+			{
+				System.out.println ("Please enter the number of columns. It can not be less then 5." );
+				input = scan.next();
+
+				if (isNumber(input) &&  Integer.parseInt(input) >= 5) 
+				{
+					cols = Integer.parseInt(input);
+					break;
+				}				
+			}
+
+			// Instantiating a ConnectFour object.
+			ConnectFour board = new ConnectFour (rows, cols); 
+			board.toString();
+			
+			// Play the game.  
+			startGame(board, rows, cols);	
+
+			// The code execution returns here after the end of the current game.
+			while(true)
+			{
+				System.out.println("Do you want to play again? Press y or q: ");
+				input = scan.next();
+
+				if( input.equals("y") || input.equals("q") )
+				{
+					break;
+				}				
+			}			
 		}	
+		
+		// Close the scanner object.
+		scan.close();
 	}
 }
